bc4ac97d803a4e463ab966194784bc4ee94eadfa
longsiyuan <mrlongsiyuan@gmail.com>
2020-01-12 16:31:17 +0800

    2

--- stage2/src/search.js
+++ stage2/src/search.js
@@ -3,14 +3,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./search.less";
-import Img from "./img/test.jpg"
+import Img1 from "./img/hash.png"
+import Img2 from "./img/seat.png"
 
 class Search extends React.Component {
 
    render(){
        return <div>
            <div className="search">search text test</div>
-           <img src={Img} />
+           <img src={Img1} />
+           <img src={Img2} />
        </div> 
    }
 }
